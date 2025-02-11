c41ac3b98410a7fcb709b14980696ec264a16c77
Francisco Pugh <franciscopugh01@gmail.com>
2025-02-11 10:53:05 -0300

    Clase 3 A

---
+++ clase-3/src/server.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import session from 'express-session'
+import MongoStore from 'connect-mongo'
+import mongoose from 'mongoose'
+import usersRouter from './routes/users.routes.js'
+import sessionsRouter from './routes/sessions.routes.js'
+
+const app = express()
+const PORT = 8080
+
+app.use(express.json())
+app.use(cookieParser("firmaSecreta"))
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: "mongodb+srv://franciscopugh01:@cluster0.jdow0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+        ttl: 60
+    }),
+    secret: "sesionSecreta",
+    resave: true,
+    saveUninitialized: true
+}))
+
+mongoose.connect("mongodb+srv://franciscopugh01:@cluster0.jdow0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+.then(() => console.log("DB is connected"))
+.catch((e) => console.log(e))
+
+app.use('/api/users', usersRouter)
+app.use('/api/sessions', sessionsRouter)
+
+app.listen(PORT, () => {
+    console.log("Server on port:", PORT);
+    
+})
